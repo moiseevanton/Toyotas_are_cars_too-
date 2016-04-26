272f2bd7a6b81dd5db4f2f2d0990657392cbd5cb
moiseevanton <athidif94@gmail.com>
2016-04-26 16:39:20 -0400

    Initial Commit

---
+++ Toyotas_are_cars_too!/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Toyotas_are_cars_too!
+//
+//  Created by Anton Moiseev on 2016-04-26.
+//  Copyright © 2016 Anton Moiseev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
